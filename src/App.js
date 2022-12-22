6e96730ce2a13ca5f87ab83884715c6138114736
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-12-22 12:43:36 +0100

    Add Context and Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { useAuthContext } from './hooks/useAuthContext';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+   const { authIsReady, user } = useAuthContext();
+
+   return (
+      <div className="App">
+         {authIsReady && (
+            <BrowserRouter>
+               <Routes>
+                  <Route
+                     path='/'
+                     element={user ? <Home /> : <Navigate to="/login" /> } />
+                  <Route
+                     path='/signup'
+                     element={!user ? <Signup /> : <Navigate to="/" />} />
+                  <Route
+                     path='/login'
+                     element={!user ? <Login /> : <Navigate to="/" />} />
+               </Routes>
+            </BrowserRouter>
+         )}
+      </div>
+   );
 }
 
 export default App;

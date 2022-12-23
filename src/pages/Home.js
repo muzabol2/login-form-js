2c226500f82a9ec85c4f9168689468027a0917ee
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-12-23 15:05:12 +0100

    Improve Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,24 @@
+import { useEffect } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from '../hooks/useLogout';
 
 export default function Home() {
    const { user } = useAuthContext();
    const { logout } = useLogout();
-   
+
+   useEffect(() => { }, [user.photoURL, user.displayName])
+
    return (
       <div className="home">
-         <h2>Home</h2>
-         <p>{user.email}</p>
-         <div className="avatar">
-            <img src={user.photoURL} alt="user avatar" />
-         </div>
+         <h2>Dzień dobry, Kierowniku złoty!</h2>
+         {user.photoURL &&
+            <div className="avatar">
+               <img src={user.photoURL} alt="user avatar" />
+            </div>
+         }
+         <p>name: {user.displayName}</p>
+         <p>mail: {user.email}</p>
          <button onClick={logout}>Logout</button>
-      </div >
+      </div>
    );
 }

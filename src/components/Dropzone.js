99e7c6bf2d305792b622acd0c961a00b7f6d803c
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-12-27 10:04:33 +0100

    Create Dropzone for avatar

---
+++ src/components/Dropzone.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { Controller, useFormContext } from 'react-hook-form';
+import './Dropzone.css';
+
+export function Dropzone({ name }) {
+   const { setValue, control } = useFormContext();
+   const [files, setFiles] = useState([]);
+   const [error, setError] = useState("");
+
+   const { getRootProps, getInputProps } = useDropzone({
+      accept: {
+         'image/*': []
+      },
+      multiple: false,
+      maxSize: 100000,
+      maxFiles: 1,
+      onDrop: (acceptedFiles, fileRejections) => {
+         console.log("acceptedFiles", acceptedFiles[0]);
+         setValue(name, acceptedFiles[0]);
+         setFiles(acceptedFiles.map(file => Object.assign(file, {
+            preview: URL.createObjectURL(file)
+         })));
+         setError("");
+         fileRejections.forEach((file) => {
+            file.errors.forEach((err) => {
+               if (err.code === "file-invalid-type") {
+                  setError(`Error: ${err.message}`);
+               }
+               if (err.code === "file-too-large") {
+                  setError(`Error: ${err.message}`);
+               }
+            });
+         });
+      }
+   });
+
+   const thumbs = files.map(file => (
+      <div className='thumb' key={file.name}>
+         <div className='thumbInner'>
+            <img
+               src={file.preview}
+               className="img"
+               onLoad={() => { URL.revokeObjectURL(file.preview) }}
+            />
+         </div>
+      </div>
+   ));
+
+   useEffect(() => {
+      return () =>
+         files.forEach(file => URL.revokeObjectURL(file.preview));
+   }, []);
+
+   return (
+      <Controller
+         control={control}
+         name={name}
+         defaultValue={[]}
+         render={() => (<>
+            <section className="drop-file-input">
+               <div {...getRootProps({ className: 'drop-file-input__label' })}>
+                  <CloudUploadIcon />
+                  <input {...getInputProps()} />
+                  <p>Drag 'n' drop your avatar image here, or click to select</p>
+                  <p>(avatar is optional)</p>
+               </div>
+            </section>
+            <aside className="thumbsContainer">
+               {thumbs}
+            </aside>
+            {error && <p className='error'>{error}</p>}
+         </>)}
+      />
+   );
+}
